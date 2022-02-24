6cc12efd299cb66d777c1adc0858c635eebe85f4
Bruno Campos <brunocu@msn.com>
2022-02-24 11:29:23 -0600

    func

--- app/src/main/java/io/github/brunocu/calculadora/MainActivity.kt
+++ app/src/main/java/io/github/brunocu/calculadora/MainActivity.kt
@@ -1,35 +1,53 @@
 package io.github.brunocu.calculadora
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+private val NUMBER = "NUMBERVIEW"
+private val HISTORY = "HISTORYVIEW"
 
 class MainActivity : AppCompatActivity() {
+    private var numberView: TextView? = null
+    private var historyView: TextView? = null
     private var hasPoint: Boolean = false
-    private var token: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        numberView = findViewById(R.id.numberView)
+        historyView = findViewById(R.id.historyView)
     }
 
     fun numberBtn(view: View) {
         if (view is Button) {
-            val numberView = findViewById<TextView>(R.id.numberView)
-            numberView.append(view.text)
+            // if last button = start new operation
+            // short-circuit eval prevents crash if text is blank
+            if (!historyView?.text.isNullOrBlank() && historyView?.text?.last() == '=') {
+                historyView?.text = ""
+                numberView?.text = ""
+            }
+            numberView?.append(view.text)
+            historyView?.append(view.text)
             // TODO remove trailing zeros?
         }
     }
 
     fun pointBtn(view: View) {
         if (view is Button && !hasPoint) {
-            val numberView = findViewById<TextView>(R.id.numberView)
-            if (numberView.text.isNullOrEmpty()) {
-                numberView.append("0.")
+            if (!historyView?.text.isNullOrBlank() && historyView?.text?.last() == '=') {
+                historyView?.text = ""
+                numberView?.text = ""
+            }
+            if (numberView?.text.isNullOrEmpty()) {
+                numberView?.append("0.")
+                historyView?.append("0.")
             } else {
-                numberView.append(".")
+                numberView?.append(".")
+                historyView?.append(".")
             }
             hasPoint = true
         }
@@ -37,21 +55,78 @@ class MainActivity : AppCompatActivity() {
 
     fun opBtn(view: View) {
         if (view is Button) {
-            val numberView = findViewById<TextView>(R.id.numberView)
-            val historyView = findViewById<TextView>(R.id.historyView)
+            // if last op = use result as first number
+            if (!historyView?.text.isNullOrBlank() && historyView?.text?.last() == '=')
+                historyView?.text = numberView?.text
+            if (view.text == "x")
+                historyView?.append("*")
+            else
+                historyView?.append(view.text)
+            numberView?.text = ""
         }
     }
 
     fun clearBtn(view: View) {
-        val numberView = findViewById<TextView>(R.id.numberView)
-        val historyView = findViewById<TextView>(R.id.historyView)
-        numberView.text = ""
-        historyView.text = ""
+        numberView?.text = ""
+        historyView?.text = ""
         hasPoint = false
     }
 
     fun eqBtn(view: View) {
         // tokenize?
-        // what if: last button is opn
+        // TODO unary operators
+        // if last button was op, wait for number
+        if (!historyView?.text.isNullOrBlank() && (historyView?.text?.last()
+                ?.isDigit() == true || historyView?.text?.last() == '.')
+        ) {
+            var currNumber = ""
+            var op: Char = '\r'
+            var calc: Float = 0F
+            for (character in historyView!!.text) {
+                if (character.isDigit() || character == '.') {
+                    // no PEMDAS - sequential interpreting
+                    // (っ˘̩╭╮˘̩)っ
+                    currNumber += character
+                } else {
+                    // do queued operation
+                    when (op) {
+                        '\r' -> calc = currNumber.toFloat()
+                        '+' -> calc += currNumber.toFloat()
+                        '-' -> calc -= currNumber.toFloat()
+                        '*' -> calc *= currNumber.toFloat()
+                        '/' -> calc /= currNumber.toFloat()
+                    }
+                    // queue next op
+                    op = character
+                    // clear working number
+                    currNumber = ""
+                }
+            }
+            // do final op
+            // not DRY :c
+            when (op) {
+                '\r' -> calc = currNumber.toFloat()
+                '+' -> calc += currNumber.toFloat()
+                '-' -> calc -= currNumber.toFloat()
+                '*' -> calc *= currNumber.toFloat()
+                '/' -> calc /= currNumber.toFloat()
+            }
+            historyView!!.append("=")
+            numberView?.text = calc.toString()
+            hasPoint = false
+        }
+    }
+
+    // save & recover values
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(NUMBER, numberView?.text.toString())
+        outState.putString(HISTORY, historyView?.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        numberView?.text = savedInstanceState.getString(NUMBER)
+        historyView?.text = savedInstanceState.getString(HISTORY)
     }
 }
